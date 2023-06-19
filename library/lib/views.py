50c65312a7afe8ea500786c13926704469682a71
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-19 18:05:21 +0330

    Add Index view

--- library/lib/views.py
+++ library/lib/views.py
@@ -1,5 +1,15 @@
+from typing import Any, Dict
 from django.shortcuts import render
+from django.views import generic
+from . import models
 
 # Create your views here.
-def index(request):
-    pass
+class Index (generic.TemplateView):
+    template_name = "lib/index.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context ['num_books'] = models.Book.objects.all().count()
+        context ['num_instances'] = models.BookInstance.objects.all().count()
+        context ['num_instances_available'] = models.BookInstance.objects.filter(status__exact='a').count()   
+        context ['num_authors'] = models.Author.objects.count()
+        return context
