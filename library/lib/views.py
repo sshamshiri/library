34bd8c94f3ecd4563a2bbe81ce8e95ce772a2c33
Sara Shamshiri <109801204+sshamshiri@users.noreply.github.com>
2023-06-19 20:07:46 +0330

    Update views.py

--- library/lib/views.py
+++ library/lib/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 from django.views import generic
 from . import models
 
-# Create your views here.
 class Index (generic.TemplateView):
     template_name = "lib/index.html"
     def get_context_data(self, **kwargs):
