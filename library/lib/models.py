8e055229f8923591d795e0d671f1aaf3e48882cc
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-19 17:14:28 +0330

    Add models to the file

--- library/lib/models.py
+++ library/lib/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+import uuid
+from django.urls import reverse
 
-# Create your models here.
+class Author(models.Model):
+    first_name    = models.CharField(max_length=100)
+    last_name     = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True , blank=True)
+    date_of_birth = models.DateField('Died', null=True , blank=True)
+
+    def get_absolute_url(self):
+        return reverse('author-detail',args=[str(self.id)])
+    
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+    
+class BookInstance(models.Model):
+    id =      models.UUIDField(primary_key=True , default=uuid.uuid4)
+    book =    models.ForeignKey('Book' , on_delete=models.SET_NULL , null=True)
+    imprint = models.CharField(max_length=200)
+    due_back= models.DateField(null=True , blank=True)
+
+    LOAN_STATUS = (
+        ('m' , 'Maintenance'),
+        ('o' , 'On loan'),
+        ('a' , 'Available'),
+        ('r' , 'Reserved')
+    )
+    status = models.CharField(max_length=1 , choices=LOAN_STATUS , blank=True  , default='m' , help_text='Book availability')
+
+    class Meta:
+        ordering = ["due_back"]
+
+    def __str__(self):
+        return f'{self.id}({self.book.title})'
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author , on_delete=models.SET_NULL , null=True)
+    summary = models.TextField(max_length=1000)
+    isbn = models.CharField('ISBN' , max_length=13 , help_text='13 character <a href="https://isbnsearch.org/">ISBN Number</a>')
+    genre = models.ManyToManyField('Genre' , help_text="Select a genre for this book")
+
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_description = 'Genre'
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200 , help_text='Enter a book genre')
+
+    def __str__(self):
+        return self.name
