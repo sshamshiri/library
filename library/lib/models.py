e67f7185b65a4a3865608d62b78aff6b93d0b4e2
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-20 16:40:16 +0330

    Edit date_of_death in models.py

--- library/lib/models.py
+++ library/lib/models.py
@@ -6,7 +6,7 @@ class Author(models.Model):
     first_name    = models.CharField(max_length=100)
     last_name     = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True , blank=True)
-    date_of_birth = models.DateField('Died', null=True , blank=True)
+    date_of_death = models.DateField('Died', null=True , blank=True)
 
     def get_absolute_url(self):
         return reverse('author-detail',args=[str(self.id)])
