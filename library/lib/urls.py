15dfb0d3c39f2ec103d6807e48d9cf2282e4efcf
sshamshiri <sarashamshiri1398@gmail.com>
2023-06-22 15:20:15 +0330

    add 'author-detail' in urls

--- library/lib/urls.py
+++ library/lib/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.Index.as_view(), name='index'),
     path('books', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
-    path('authors', views.AuthorListView.as_view(), name='authors')
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author_detail')
 ]
